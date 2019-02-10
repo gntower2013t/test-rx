2679515a7fcca10b18363f4e9334bf4fc9719b66
gntower2013t <chensg2008@gmail.com>
2019-02-11 00:00:10 +0800

    home

--- rx-no-dom/temp-run.ts
+++ rx-no-dom/temp-run.ts
@@ -1,5 +1,24 @@
-console.log('===我们' +
+// console.log('===我们' +
+// // /\/multi-mon\/.*\.(?!js$)/.exec('/multi-mon/patient.cjs')
+// /\/multi-mon\/.*\.(?!.*\.js$|.*\.css$)/.exec('/multi-mon/styles.4a657eed7ccf7d3d30ff.js')
+//   // /\d+(?!\.)/.exec('3.141')
+// )
+
+
+/* console.log('===我们' +
 // /\/multi-mon\/.*\.(?!js$)/.exec('/multi-mon/patient.cjs')
 /\/multi-mon\/.*\.(?!.*\.js$|.*\.css$)/.exec('/multi-mon/styles.4a657eed7ccf7d3d30ff.js')
   // /\d+(?!\.)/.exec('3.141')
-)
+) */
+
+
+// let path = "/wado/fhir/eiweb/Patient?_format=json&_pretty=true&name=PKDBB185";
+// let pp =path.replace(/^\/wado\/fhir\/eiweb.*/, "$&&user=agfa/agfa&password=1.xero");
+// console.log(
+//   " pp === " + pp
+// );
+
+
+// if ("0") {
+//   console.log("string 0 is true, number 0 is false");
+// }
--- rx-no-dom/test-new.ts
+++ rx-no-dom/test-new.ts
@@ -1,5 +1,5 @@
-import { of, Observable, defer, combineLatest } from "rxjs";
-import { tap, shareReplay, map,  } from 'rxjs/operators';
+import { of, Observable, defer, combineLatest, interval, empty } from "rxjs";
+import { tap, shareReplay, map, take, switchMap, share, delay,  } from 'rxjs/operators';
 
 const emptyS = () => { };
 
@@ -12,23 +12,43 @@ const tSub = (obr, obs) => {
   });
 }
 
-/* test share replay */
-const x =defer(() => Promise.resolve("XXX"))
-const y =defer(() => Promise.resolve("YYY"))
-const source =
-  combineLatest(x,y).pipe(map(([x,y])=> x+","+y))
-  // of("XXX", "YYY")
-  .pipe(tap(() => console.log("=========start!!=====")));
-const re = source.pipe(shareReplay(1));
+/* test share replay, defer */
+// const x =defer(() => Promise.resolve("XXX"))
+// const y =defer(() => Promise.resolve("YYY"))
+// const source =
+//   combineLatest(x,y).pipe(map(([x,y])=> x+","+y))
+//   // of("XXX", "YYY")
+//   .pipe(tap(() => console.log("=========start!!=====")));
+// const re = source.pipe(shareReplay(1));
 
-let sup = re.subscribe(obs("a"));
-/* setTimeout(() => {
-  sup.add(re.subscribe(obs("b")));
-  tSub(re, obs("c"));
-}); */
-// tSub(re, obs('b0'))
-sup.add(re.subscribe(obs("b")));
+// let sup = re.subscribe(obs("a"));
+// /* setTimeout(() => {
+//   sup.add(re.subscribe(obs("b")));
+//   tSub(re, obs("c"));
+// }); */
+// // tSub(re, obs('b0'))
+// sup.add(re.subscribe(obs("b")));
 
-// console.log(sup.closed);
+// // console.log(sup.closed);
 
-re.subscribe(obs("c"));
+// re.subscribe(obs("c"));
+
+
+/* switchMap with empty */
+interval(500).pipe(
+  take(10),
+  switchMap(v => {
+    if (v % 3 === 0) {
+      // return empty() //相当于 filter
+      return of(v)
+    }
+    return of(v).pipe(delay(600))
+  }),
+  tap(v=>console.log("======"))
+).subscribe(obs("ss"))
+
+
+/* empty pipe */
+// let i = interval(500);
+// let j = i.pipe();
+// console.log(i===j);
